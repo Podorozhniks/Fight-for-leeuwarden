6716fbcfe8adcbe17458eb4b9087ba5105002f58
Nm1mp <cherepalex04@gmail.com>
2024-04-03 14:53:59 +0200

    7th commit

---
+++ Fight_for_Leeuwarden/Assets/EnemySpawner.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyPrefab; 
+    [SerializeField] private float spawnDelay = 1f; 
+    [SerializeField] private int numberOfEnemies = 5; 
+    private int currentEnemiesSpawned = 0; 
+    private Coroutine spawningCoroutine;
+
+    void Start()
+    {
+        
+        spawningCoroutine = StartCoroutine(SpawnEnemies());
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        while (currentEnemiesSpawned < numberOfEnemies)
+        {
+            
+            Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            currentEnemiesSpawned++;
+
+         
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+
+    // Stop Spawning
+    public void StopSpawning()
+    {
+        if (spawningCoroutine != null)
+        {
+            StopCoroutine(spawningCoroutine);
+        }
+    }
+}
