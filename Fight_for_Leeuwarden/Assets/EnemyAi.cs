536527f2421814e6c43baa06cd9d2e9c36fec8ab
Nm1mp <cherepalex04@gmail.com>
2024-04-06 21:30:55 +0200

    Main commit2

--- Fight_for_Leeuwarden/Assets/EnemyAi.cs
+++ Fight_for_Leeuwarden/Assets/EnemyAi.cs
@@ -21,7 +21,6 @@ public class EnemyAi : MonoBehaviour
     public float timeBetweenAttacks;
     bool alreadyAttacked;
     public GameObject projectile;
-    public int damage = 50;
 
     //States
     public float sightRange, attackRange;
@@ -76,18 +75,17 @@ public class EnemyAi : MonoBehaviour
 
     private void AttackPlayer()
     {
-        
+        ///Make sure enemy doesn't move
         agent.SetDestination(transform.position);
 
         transform.LookAt(player);
 
         if (!alreadyAttacked)
         {
-            
+            ///Attack code here
             Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
             rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
             rb.AddForce(transform.up * 8f, ForceMode.Impulse);
-
             ///End of attack code
 
             alreadyAttacked = true;
